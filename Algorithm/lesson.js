a1f0a4e382ec55e03789aa4c7cd69615da8d0b32
Daniel Samuel <dansam5000@gmail.com>
2022-08-10 13:21:51 +0100

    Introduction tpo algorithm

---
+++ Algorithm/lesson.js
@@ -0,0 +1,9 @@
+// Write an algorithm to print out our names and how many of us are present in the lectures today
+
+// I will need attendance list of people that are presnt
+
+// function to take attendance list
+
+// A function that will go through the list  and print out a specific name
+
+// I will now write another function that will print out the total number of student present
