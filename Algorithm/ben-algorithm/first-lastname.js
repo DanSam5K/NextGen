fda4e56d726dc5038c5020b060b3c29071fb8498
Benjamin Chisom <chukwubenjamin77@gmail.com>
2022-08-17 02:37:52 +0100

    algorithm to multiply 3 number and also add names

---
+++ Algorithm/ben-algorithm/first-lastname.js
@@ -0,0 +1,23 @@
+// step 1 start
+
+// step 2. Get knowledege of my input: firstname,  and result
+
+// step 3. declare firstname, lastname and result
+
+// step 4. take input for firstname, lastname from user
+
+// step 5 firstname + lastname---> result
+
+// step 6. print out result
+
+// step 7. End the program
+
+const multiplyTwoNum = (firstname, lastname) => {
+  return (result = firstname + lastname);
+};
+
+const printResult = (result) => {
+  console.log(result);
+};
+
+printResult(multiplyTwoNum(chukwu, benjamin));
