5310f7d9c1f9e1bb70fcdcbe49587318fb0940ca
Daniel Samuel <dansam5000@gmail.com>
2022-09-16 17:10:14 +0100

    Add index.js entry point of app

---
+++ react-project/tutor-todo-app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>Hello, world! our first react application</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
