5ee74479076eb9dfcd93aaad7532085abc5e8e8d
MccGrey <chuxzzie01@yahoo.com>
2022-09-17 11:00:40 +0100

    set up components

---
+++ react-project/mccgrey-todo-app/src/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/nav";
+
+const app = () => {
+  return (
+    // REACT FRAGMENTS
+    <>
+      <Header />
+      <Nav />
+    </>
+  );
+};
+
+export default app;
