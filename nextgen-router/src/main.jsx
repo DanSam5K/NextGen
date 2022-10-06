f2187493730604e5ca8b057286b5caf96727d3b8
Daniel Samuel <dansam5000@gmail.com>
2022-10-06 09:50:20 +0100

    Revert "Task outline"

--- nextgen-router/src/main.jsx
+++ nextgen-router/src/main.jsx
@@ -1,28 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
-import "./index.css";
-import Root from "./routes/root";
-import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-
-    children: [
-      {
-        path: "contacts/:contactId",
-        element: <Contact />,
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
-);
+)
