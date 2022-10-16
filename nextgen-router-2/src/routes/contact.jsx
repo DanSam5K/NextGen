4b09c6e4d578010ee7c250032ec328bbd865814e
MccGrey <chuxzzie01@yahoo.com>
2022-10-16 04:54:43 +0100

    updated

--- nextgen-router-2/src/routes/contact.jsx
+++ nextgen-router-2/src/routes/contact.jsx
@@ -9,14 +9,7 @@ export async function action({ request, params }) {
 }
 
 export async function loader({ params }) {
-  const contact = await getContact(params.contactId);
-  if (!contact) {
-    throw new Response("", {
-      status: 404,
-      statusText: "Not Found",
-    });
-  }
-  return contact;
+  return getContact(params.contactId);
 }
 
 export default function Contact() {
