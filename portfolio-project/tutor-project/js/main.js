011266feea2b02dcce5182f741a4d0b7467658a8
Daniel Samuel <dansam5000@gmail.com>
2022-10-17 08:42:31 +0100

    add hamburger menu  & toggle HM

---
+++ portfolio-project/tutor-project/js/main.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector(".menu-btn");
+const hamburger = document.querySelector(".menu-btn__burger");
+
+let showMenu = false;
+
+menu.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    hamburger.classList.add("open");
+    showMenu = true;
+  } else {
+    hamburger.classList.remove("open");
+    showMenu = false;
+  }
+}
